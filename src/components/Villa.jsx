7d42e7f2f598598a3f63a926388a495d31a7674a
JonMallia <jon@jonmallia.mt>
2025-06-07 14:15:55 +0200

    Create Villa.jsx

---
+++ src/components/Villa.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Gallery from './Gallery'
+
+function Villa() {
+  return (
+    <div>
+      <h2>Villa Page</h2>
+      <p>Welcome to the Respiro Villa.</p>
+      <Gallery />
+    </div>
+  )
+}
+
+export default Villa
